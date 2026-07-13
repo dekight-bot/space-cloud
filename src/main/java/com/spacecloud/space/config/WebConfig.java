b2fcfa3f63b42d7a5c4118fb49cf6ad9dd1dffd8
dekight <dekight@naver.com>
2026-07-14 02:55:54 +0900

    이미지 경로 설정. webConfig

--- src/main/java/com/spacecloud/space/config/WebConfig.java
+++ src/main/java/com/spacecloud/space/config/WebConfig.java
@@ -15,6 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 브라우저에서 /images/** 주소로 들어오면 C:/upload/ 폴더로 연결해주는 핵심 설정
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///" + uploadDir);
+                .addResourceLocations("file:/app/upload");
     }
 }
